4fe568d10c206df114419d2040fd88dcb2e03b1c
Victor102801 <victorch102801@gmail.com>
2025-03-18 15:56:22 -0600

    Cargando archivos iniciales

---
+++ app.js
@@ -0,0 +1,8 @@
+const amigos = []; // Array para almacenar los nombres de amigos
+
+// Función para agregar un amigo a la lista
+function agregarAmigo() {
+    const input = document.getElementById("amigo"); // Capturar el valor del input
+    const nombre = input.value.trim(); // Eliminar espacios en blanco al inicio y al final
+}
+
